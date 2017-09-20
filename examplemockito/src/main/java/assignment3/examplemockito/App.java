9c257c3ef2ba1c3c41e37178379815e259ab5c55
shanini.kumari <shanini.kumari@B102-14.soit.local>
2017-09-20 12:23:57 +1200

    This is first part of individual Assignment 3

---
+++ examplemockito/src/main/java/assignment3/examplemockito/App.java
@@ -0,0 +1,13 @@
+package assignment3.examplemockito;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
